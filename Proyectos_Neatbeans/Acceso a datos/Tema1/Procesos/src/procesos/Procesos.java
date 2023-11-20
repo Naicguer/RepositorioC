446859d51f35588f0edbc1820922df99ac0eda8a
Naicguer <Naicguer@gmail.com>
2023-11-20 12:42:01 +0100

    procesos t3

--- Proyectos_Neatbeans/Acceso a datos/Tema1/Procesos/src/procesos/Procesos.java
+++ Proyectos_Neatbeans/Acceso a datos/Tema1/Procesos/src/procesos/Procesos.java
@@ -1,13 +1,33 @@
 package procesos;
 
+import java.io.*;
 
 public class Procesos {
 
     
     public static void main(String[] args) {
     
-        Thread Thread = Thread; 
+        String ruta = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
         
+        ProcessBuilder ejecutar = new ProcessBuilder(ruta);
+        
+        try{
+            
+           Process proceso=ejecutar.start();
+           int devolver = proceso.waitFor();
+           System.out.println("La ejecucion de "+ruta+" devuelve "+devolver);
+           
+        }catch(IOException e){
+            
+            System.err.println("Error"+e.toString());
+            System.exit(-1);
+            
+        }catch(InterruptedException e){
+            
+            System.err.println("El proceso hijo afinalizo de forma iuncorrecta");
+            System.exit(-1);
+            
+        }
         
         
     }
