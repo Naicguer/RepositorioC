3854f8288843b13792fd1a62165200f330f6d447
Naicguer <Naicguer@gmail.com>
2023-11-29 23:20:21 +0100

    t5 fin

---
+++ Proyectos_Neatbeans/Acceso a datos/myshopp/src/main/java/com/funcionaconno/myshopp/MyshoppApplication.java
@@ -0,0 +1,13 @@
+package com.funcionaconno.myshopp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshoppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshoppApplication.class, args);
+	}
+
+}
---
+++ Proyectos_Neatbeans/Acceso a datos/myshopp/src/test/java/com/funcionaconno/myshopp/MyshoppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funcionaconno.myshopp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyshoppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
